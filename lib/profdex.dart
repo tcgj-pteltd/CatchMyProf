03d97e1ca9c35258f2e70f3546f09dc11165b0e5
jon-chua <jonchua97@gmail.com>
2021-01-08 18:34:12 +0800

    More UI changes

--- lib/profdex.dart
+++ lib/profdex.dart
@@ -12,6 +12,36 @@ class _ProfDexState extends State<ProfDex> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: new Container(
+            margin: const EdgeInsets.only(top: 10.0),
+            child: new Row(
+                children: <Widget>[
+                  Expanded(
+                      child: new Container(
+                          margin: const EdgeInsets.only(right: 20.0),
+                          child: Divider(
+                            color: Color(0xFF92140C),
+                            thickness: 4,
+                          )
+                      )
+                  ),
+                  Text('CATCH MY PROF', style: TextStyle(fontSize: 23.0, fontWeight: FontWeight.bold, letterSpacing: 1.5)),
+                  Expanded(
+                      child: new Container(
+                          margin: const EdgeInsets.only(left: 20.0),
+                          child: Divider(
+                            color: Color(0xFF92140C),
+                            thickness: 4,
+                          )
+                      )
+                  )
+                ]
+            )
+        ),
+        elevation: 0,
+      ),
       body: Center(
         child: Column(
           children: <Widget>[
